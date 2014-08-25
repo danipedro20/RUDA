8a01115381587ef41f1fdc48d98fce2c33f3cc25
danipedro20 <danipedro23@gmail.com>
2014-08-25 14:55:08 -0400

    cambios en la estructura html

---
+++ application/views/front_end/usuarios_view.php
@@ -0,0 +1,12 @@
+<section class="contenido">
+
+        <h1>Iniciar Sesion</h1>
+        <form>
+            <label for="Usuario">Usuario</label>
+            <input type="text" name="user"/> <br/>
+            <label for="Contraseña">Contraseña</label>
+            <input type="password" name="pass"/> <br/>
+            <input type="submit" value="Entrar" name="submit"/>     
+    </body>
+
+</section>
