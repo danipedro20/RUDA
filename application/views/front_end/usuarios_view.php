6ca9edba75d3960a2f35269f9ffece6dc3df0398
danipedro20 <danipedro23@gmail.com>
2014-08-25 16:55:51 -0400

    faltaba etiqueta de cierre del form

--- application/views/front_end/usuarios_view.php
+++ application/views/front_end/usuarios_view.php
@@ -3,11 +3,15 @@
         
         <form>
             <h2>Iniciar Sesion</h2>
+            
             <label for="Usuario">Usuario</label>
             <input type="text" name="user"/> <br/>
+            
             <label for="Contraseña">Contraseña</label>
             <input type="password" name="pass"/> <br/>
-            <input type="submit" value="Entrar" name="submit"/>     
-    </body>
+            
+            <input type="submit" value="Entrar" name="submit"/>
+        </form>
+    
 
 </section>
