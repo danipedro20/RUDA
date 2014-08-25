623daab85a3042d0ec35c1cb180f89f1fa327235
danipedro20 <danipedro23@gmail.com>
2014-08-25 16:21:34 -0400

    cambio de direccionamiento de la carpeta del archivo

---
+++ application/controllers/backend/adhome.php
@@ -0,0 +1,19 @@
+<?php
+//AQUI INICIA EL CONTROLADOR DE LA PAGINA admin
+class Adhome extends CI_Controller {
+    //Se crea siempre un constructor al inicial un controlador
+    public function __construct() {
+        parent::__construct();
+        //llamado al modelo
+        $this->load->model('/backend/Perfil_model');
+    }
+    
+    function index2() {
+
+        $datos['perfiles'] = $this->Perfil_model->perfiles();
+        $datos['titulo'] = 'Ruda - Administración';
+        $datos['contenido'] = 'adindex';
+        $this->load->view('plantillas/adplantilla', $datos);
+    }
+
+}  
