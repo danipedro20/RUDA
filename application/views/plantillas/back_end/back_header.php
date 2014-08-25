d5ea1447053f776f49299c15522e8dc292f9b093
danipedro20 <danipedro23@gmail.com>
2014-08-25 14:51:01 -0400

    cambios de nombre a los archivos de estilo css

--- application/views/plantillas/back_end/back_header.php
+++ application/views/plantillas/back_end/back_header.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <title>Gestión de Aula</title>
-        <link href="http://ruda.edu.py/assets/back_end/css/estilos.css" rel="stylesheet" type="text/css">
+        <link href="http://ruda.edu.py/assets/back_end/css/backestilos.css" rel="stylesheet" type="text/css">
     </head>
     <body>
         <header>
@@ -15,12 +15,12 @@
         <nav>
             <section class="contenedor">
                 <ul>
-                    <li><a href="http://ruda.edu.py/admin/adhome/index2">Inicio</a></li>
+                    <li><a href="http://ruda.edu.py/backend/adhome/index2">Inicio</a></li>
                     <li><a href="#">Carreras</a></li>
                     <li><a href="#">Aulas</a></li>
                     <li><a href="#">Profesores</a></li>
                     <li><a href="#">Tareas</a></li>
-                    <li><a href="http://ruda.edu.py/home/index">Cerrar Sesión</a></li>
+                    <li><a href="http://ruda.edu.py/frontend/home/index">Cerrar Sesión</a></li>
                 </ul>
             </section>
         </nav>
