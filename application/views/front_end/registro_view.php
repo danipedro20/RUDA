203839405363eeef58b28e604573ea154a5fcd19
danipedro20 <danipedro23@gmail.com>
2014-08-25 14:55:25 -0400

    cambios en la estructura html

---
+++ application/views/front_end/registro_view.php
@@ -0,0 +1,11 @@
+<section class="contenido">
+
+        <h1>Registrarse</h1>
+        <form>
+            <label for="Usuario">Usuario</label>
+            <input type="text" name="user"/> <br/>
+            <label for="Contraseña">Contraseña</label>
+            <input type="password" name="pass"/> <br/>
+            <input type="submit" value="Registrame" name="submit"/>     
+        </form>
+</section>
