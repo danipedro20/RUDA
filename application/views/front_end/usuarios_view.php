8c1425c7ff3e489a51e9fa36a6d7918417d08f8f
danipedro20 <danipedro23@gmail.com>
2014-08-26 16:50:43 -0400

    cambios en el formulario de sesion de usuario

--- application/views/front_end/usuarios_view.php
+++ application/views/front_end/usuarios_view.php
@@ -1,21 +1,16 @@
 <section class="contenido">
-    <html>
-        <header>
-            <meta charset="utf-8"/>
-            <title></title>
-        </header>
-        <body>
-            <h1>Iniciar Sesion</h1>
-            <form name="form_inicio" action="<?= base_url() . 'frontend/usuarios_control/inicio_very' ?>" method="POST">
-                <label for="Usuario">Usuario</label>
-                <input type="text" name="usunombre"/> <br/>
-                <label for="Contraseña">Contraseña</label>
-                <input type="password" name="usupass"/> <br/>
-                <input type="submit"value="Entrar"name="submit_inicio"/>     
-        </body>
 
-        </form>
-        <hr />
-        <?= validation_errors(); ?>
-    </html>
+    
+    <form name="form_inicio" action="<?= base_url() . 'frontend/usuarios_control/inicio_very' ?>" method="POST">
+        <h2>Iniciar Sesion</h2>
+        <label for="Usuario">Usuario</label>
+        <input type="text" name="usunombre"/> <br/>
+        <label for="Contraseña">Contraseña</label>
+        <input type="password" name="usupass"/> <br/>
+        <input type="submit"value="Entrar"name="submit_inicio"/>     
+
+    </form>
+   
+    <?= validation_errors(); ?>
+
 </section>
