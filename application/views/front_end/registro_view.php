29b8b9e5aa9c62b0a0edfbc5fd9dc4ee6e0dc333
danipedro20 <danipedro23@gmail.com>
2014-08-26 12:02:09 -0400

    cambio en la vista en donde estaba mal el codigo html que insertaba los titulos y los labels al formulario

--- application/views/front_end/registro_view.php
+++ application/views/front_end/registro_view.php
@@ -1,20 +1,16 @@
 <section class="contenido">
 
-<<<<<<< HEAD
+
     <form>
         <h2>Registrarse</h2>
-        
-        <label for="usunombre">Nombre:</label>
-        <input type="text" name="usunombre"/> <br/>
-=======
-    <h1>Registrarse</h1>
+               
     <?php if (isset($data)): ?>
         <h2>$data</h2>
     <?php endif; ?>
     <form name="form_registro" action="<?= base_url() . 'frontend/registro_control/registro_very' ?>" method="POST">
-        <label for="usunombre"><?= @set_value('usunombre') ?>":</label>
+        
+        <label for="usunombre">Nombre:</label>
         <input type="text" name="usunombre" value=" <?= @set_value('usunombre') ?>"/> <br/>
->>>>>>> 19def7aa8f496f7c0917d653a2c931e3eb2389a9
 
         <label for="usucedula">C.I. Nro.:</label>
         <input type="text" name="usunrocedula" value=" <?= @set_value('usunrocedula') ?>"/> <br/>
@@ -27,21 +23,6 @@
 
         <label for="usuemail">Email:</label>
         <input type="email" name="usuemail" value=" <?= @set_value('usuemail') ?>"/> <br/>
-
-        <!--        <label for="usuperfil">Perfil:</label>
-                <select name="idperfil"> 
-                    <option value=" //">Alumno</option>
-                    <option value=" //">Profesor</option>
-                    <option value=" //>">Administrador</option>
-                </select> <br/>
-        
-                <label for="usuturno">Turno:</label>
-                <select name="idturno"> 
-                    <option value="1">Mañana</option>
-                    <option value="2">Tarde</option>
-                    <option value="3">Noche</option>
-        
-                </select><br/>-->
 
         <label for="Contraseña">Contraseña</label>
         <input type="text" name="usupassword" value=" <?= @set_value('usupassword') ?>"/> <br/>
