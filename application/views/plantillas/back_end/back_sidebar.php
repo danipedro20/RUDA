6b09b60bfefd4f0f8436bfeec31a70a70e379117
danipedro20 <danipedro23@gmail.com>
2014-08-28 17:26:53 -0400

    se agrego la url de acceso al controller de lista

--- application/views/plantillas/back_end/back_sidebar.php
+++ application/views/plantillas/back_end/back_sidebar.php
@@ -2,15 +2,16 @@
     <ul>
         <li>Aulas</li>
         <li><a href="http://ruda.edu.py/backend/reg_aula/nueva_aula">Crear Aulas</a></li>
-        <li><a href="#">Plan de Estudios</a></li>
+        <li><a href="http://ruda.edu.py/backend/reg_plan/RegistrarPlan">Plan de Estudios</a></li>
         <li><a href="#">Profesores</a></li>
         <li><a href="#">Alumnos</a></li>
         <li><a href="#">Tareas</a></li>
     </ul>
     <ul>
         <li>Usuarios</li>
-        <li><a href="#">Registrar</a></li>
-        <li><a href="#">Listar</a></li>
+        <li><a href="http://ruda.edu.py/backend/reg_usuario/registrousuario">Registrar</a></li>
+        <li><a href="#">Perfiles</a></li>
+        <li><a href="http://ruda.edu.py/backend/listarusuarios/listarusuario">Listar</a></li>
         <li><a href="#">Asignar Aulas</a></li>
         <li><a href="#">Asignar Tareas</a></li>
     </ul>
