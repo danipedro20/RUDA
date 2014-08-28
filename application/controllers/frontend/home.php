adb518399f19c7449a03dddccce90c50cf372924
danipedro20 <danipedro23@gmail.com>
2014-08-28 17:26:30 -0400

    se quito la linea que llamaba a un modelo que no estaba siendo utilizada

--- application/controllers/frontend/home.php
+++ application/controllers/frontend/home.php
@@ -4,11 +4,11 @@ class Home extends CI_Controller {
     //Se crea siempre un constructor al inicial un controlador
     public function __construct() {
         parent::__construct();
-        $this->load->model('frontend/Carrera_model');
+        
     }
     //Este metodo esta llamando a la pagina principal el cual es la vista index
     function index() {
-        $datos['carreras'] = $this->Carrera_model->lista_usuario();
+        
         $datos['titulo'] = 'Ruda - Gestion de Aula';
         $datos['contenido'] = 'index';
         $this->load->view('plantillas/plantilla', $datos);
