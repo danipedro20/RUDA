087412ffe8096c9a666c9eb79fe8122abe734d6b
danipedro20 <danipedro23@gmail.com>
2014-08-28 17:31:18 -0400

    controller para el registro de usuario

--- application/controllers/frontend/registro_control.php
+++ application/controllers/frontend/registro_control.php
@@ -5,7 +5,7 @@ class Registro_control extends CI_Controller {
     public function __construct() {
         parent::__construct();
 
-        $this->load->model('registro_model');
+        $this->load->model('frontend/registro_model');
     }
 
     public function registro() {
@@ -20,16 +20,16 @@ class Registro_control extends CI_Controller {
             $this->form_validation->set_rules('usunombre', 'Nombre', 'required|trim|callback_very_user');
             $this->form_validation->set_rules('usunrocedula', 'Número de cédula', 'required');
             $this->form_validation->set_rules('usudireccion', 'Dirección', 'required');
-              $this->form_validation->set_rules('usutelefono', 'telefono', 'required');
+            $this->form_validation->set_rules('usutelefono', 'telefono', 'required');
             $this->form_validation->set_rules('usuemail', 'Correo', 'required');
             $this->form_validation->set_rules('usupassword', 'Password', 'required');
-            $this->form_validation->set_message('very_user','El Usuario con ese  %s ya existe');
+            $this->form_validation->set_message('very_user', 'El Usuario con ese  %s ya existe');
             if ($this->form_validation->run() != FALSE) {
                 $this->registro_model->add_usuario();
                 $data = array('mensaje' => 'El Usuario se ha Registrado');
-                  $datos['titulo'] = 'Ruda - Inicio Sesion';
-                 $datos['contenido'] = 'registro_view';
-                $this->load->view('plantillas/plantilla', $data,$datos);
+                $datos['titulo'] = 'Ruda - Inicio Sesion';
+                $datos['contenido'] = 'registro_view';
+                $this->load->view('plantillas/plantilla', $data, $datos);
             } else {
                 $datos['titulo'] = 'Ruda - Inicio Sesion';
                 $datos['contenido'] = 'registro_view';
