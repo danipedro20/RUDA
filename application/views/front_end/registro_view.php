81e5db9fc0432a2f0c6ac3f0e963396756848df7
ruben <toledorubendario91@gmail.com>
2015-04-20 22:08:56 -0400

    Actualizacion de la vista del registro

--- application/views/front_end/registro_view.php
+++ application/views/front_end/registro_view.php
@@ -10,11 +10,15 @@ header("Cache-Control: no-store, no-cache, must-revalidate");
     }
 </script>
 <section class="contenido">
-<fieldset>
+    <fieldset>
         <h1>Registrarse</h1>
-        <?php echo form_open("frontend/registro_control/insertarregi") ?>
+        <?php if ($_SERVER['HTTP_REFERER'] != base_url('/frontend/registro_control/inscripcion')) { ?>
+            <h3 ALIGN = CENTER>Si aun no estas inscripto, Inscribete <a href = "<?php echo base_url() ?>frontend/registro_control/inscripcion">Aqui</a></h3>
+        <?php } ?>
+        <?php echo form_open("frontend/registro_control/insertarregi")
+        ?>
 
-        <label name="lbl_usu_nombre">Nombre: </label>
+        <label name="lbl_usu_nombre">Nombre/s y Apellido/s: </label>
         <input type="text" name="usu_nombre" placeholder="Nombre" required="" value='<?php echo set_value('usu_nombre') ?>' />
         <label name="lbl_usu_nrocedula">Número de C.I: </label>
         <input type="text" name="usu_nrocedula" placeholder="Número de C.I" required="" value='<?php echo set_value('usu_nrocedula') ?>' />
@@ -25,25 +29,20 @@ header("Cache-Control: no-store, no-cache, must-revalidate");
         <label name="lbl_usu_email">Email: </label>
         <input type="email" name="usu_email" placeholder="Email" required="" value='<?php echo set_value('usu_email') ?>' />
         <label name="lbl_usu_pass">Contraseña: </label>
-         <input type="password" name="usu_pass" placeholder="Contraseña" required="" value='<?php echo set_value('usu_password') ?>' />
-         <label for="idturno">Turno:</label>
-         <select name="idturno" id="idturno" required="">
-            <option value="" selected="selected">Seleccione un Turno</option>
-            <option value="1" <?php echo set_select('idturno','1');?>>Mañana</option>
-            <option value="2" <?php echo set_select('idturno','2');?>>Tarde</option>
-            <option value="3" <?php echo set_select('idturno','3');?>>Noche</option>
-        </select> 
+        <input type="password" name="usu_pass" placeholder="Contraseña" required="" value='<?php echo set_value('usu_password') ?>' />
+
         <label name="lbl_pregunta">Pregunta Secreta: </label>
         <input type="text" name="pregunta" placeholder="Pregunta Secreta" required="" value='<?php echo set_value('pregunta') ?>' />
         <label name="respuesta">Respuesta Secreta: </label>
-        <input type="text" name="respuesta" placeholder="Respuesta Secreta" required="" value='<?php echo set_value('respuesta') ?>' />             
+        <input type="text" name="respuesta" placeholder="Respuesta Secreta" required="" value='<?php echo set_value('respuesta') ?>' />  
+
         <!-- este input lo usaremos como referencia para la validacion -->
         <input type="hidden" name="grabar" value="si" />
 
         <!--            En esta linea estamos configurando como se va a mostrar el error si algo no esta bien-->
         <font color='red' style='font-weight: bold; font-size: 14px; text-decoration: underline'><?php echo validation_errors(); ?></font>
 
-        <input type="submit" name="btnguardarplan" value="Guardar" />
+        <input type="submit" name="btnguardarplan" value="Registrarse" />
 
         <?php echo form_close() ?> 
 
