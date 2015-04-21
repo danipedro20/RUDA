5cc46ddd0ddb88955ad0c011856304919d4799ae
ruben <toledorubendario91@gmail.com>
2015-04-20 23:35:27 -0400

    agregando vista de inscripción
    se agrego las vistas insvripcion_view y registrofail_view

---
+++ application/views/front_end/registrofail_view.php
@@ -0,0 +1,16 @@
+<?php
+header("Expires: Thu, 19 Nov 1981 08:52:00 GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate");
+?>
+<meta http-equiv="Expires" content="0" /> 
+<meta http-equiv="Pragma" content="no-cache" />
+<script Language="JavaScript">
+    if (history.forward(-1)) {
+        history.replace(history.forward(-1));
+    }
+</script>
+<section class="contenido">
+
+    <h2 ALIGN = CENTER>Verifique el Nombre de Usuario si piensa que ya esta inscripto</h2>
+    <h3 ALIGN = CENTER>Si aun no estas inscripto, Inscribete <a href = "<?php echo base_url() ?>frontend/registro_control/inscripcion">Aqui</a></h3>
+</section>
