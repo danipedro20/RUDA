aa25c9e37322a999b598d0f943f46ae15e87c86f
ruben <toledorubendario91@gmail.com>
2015-10-23 09:52:51 -0300

    Cambios al 23/10/2015
    Cambios generales en todas las seccione

--- application/views/plantillas/back_end/back_footer_alumno.php
+++
@@ -1,9 +0,0 @@
-</section>
-<footer>
-    <section class="contenedor">
-        <p>En esta seccion de la pagina van los créditos de la web <a href="http://todoprogramacion.com.ve/cursos/codeigniter">Gestion de Aula RUDA</a> desde <a href="http://todoprogramacion.com.ve">ruda.edu.py</a></p>
-        <p>Se tomaron estilos e imágenes del framework CSS <a href="http://getbootstrap.com" rel="nofollow">Bootstrap 3</a> de Twitter</p>
-    </section>
-</footer>
-</body>
-</html>
--- application/views/plantillas/profplantilla.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$this->load->view('plantillas/back_end/back_header_profesor');
-$this->load->view('plantillas/back_end/back_sidebar_profesor');
-$this->load->view('back_end/'. $contenido);
-$this->load->view('plantillas/back_end/back_footer_profesor');
